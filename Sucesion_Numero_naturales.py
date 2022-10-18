77151475afb8fb443ccbd7458c319374a4b54242
DylanWreck <89149910+DylanWreck@users.noreply.github.com>
2022-10-17 23:13:08 -0400

    Update Sucesion_Numero_naturales.py
    Escribir un programa que le pida al usuario que ingrese una sucesión de números naturales
    (primero uno, luego otro, y así hasta que el usuario ingrese ’-1’ como condición de salida). Al final, el
    programa debe imprimir cuántos números fueron ingresados, la suma total de los valores y el promedio.

--- Sucesion_Numero_naturales.py
+++ Sucesion_Numero_naturales.py
@@ -1,7 +1,3 @@
-#Escribir un programa que le pida al usuario que ingrese una sucesión de números naturales
-#(primero uno, luego otro, y así hasta que el usuario ingrese ’-1’ como condición de salida). Al final, el
-#programa debe imprimir cuántos números fueron ingresados, la suma total de los valores y el promedio.
-
 import time
 
 print("Sucesion de numero naturales.")
